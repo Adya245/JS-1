11f4bb080bf7cad7b1628b20c966b2d25812f986
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-02-06 23:19:00 +0530

    Add files via upload

---
+++ Day1/JS Introduction/app.js
@@ -0,0 +1,37 @@
+console.log("Hello from console.log() in app.js file");
+console.table(["Audi", "Volvo", "Ford"])
+console.table({firstname:"J0HN", lastname:"DOE"})
+
+console.group();
+console.log("Hello again, this time inside a group");
+console.log("Hello again, this time inside a group");
+console.log("Hello again, this time inside a group");
+console.groupEnd()
+console.log("Hello ji");
+
+console.warn("Nahi bataunga")
+
+console.error("Kya karna hai")
+
+var name="Adya"
+console.log(name);
+
+var name="Richa"
+console.log(name)
+
+var age=22
+console.log(age);
+
+age=25
+console.log(age);
+
+let x=40256
+console.log(x);
+
+x=78965
+console.log(x);
+
+const y = 70
+console.log(y);
+
+
