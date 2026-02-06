48f4abccf7c96b99ec6b5e6b2e540a3695817b04
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-02-06 23:21:17 +0530

    Add files via upload

---
+++ Day2/Datatypes/app.js
@@ -0,0 +1,60 @@
+var a = 32
+var b = 55.72
+var c = -7
+var d = "abcd"
+var e = null
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.log(typeof a);
+console.log(typeof d);
+console.log(typeof e);
+
+
+var num = 45.5555555555
+console.log(num.toFixed(5));
+console.log(num.toString());
+
+// strings
+
+var a = 'jjhdbf'
+var b = "abjjhsd"
+var c = `178839`
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+var naam = "Adya"
+var age = 22
+
+console.log(`Hello my name is ${naam} and my age is ${age}`);
+
+//Boolean
+
+var x = true
+var y = false
+
+console.log(x);
+console.log(y);
+console.log(typeof x);
+console.log(typeof y);
+
+
+var s = "Hello world"
+var s2 = 'kaise ho'
+
+console.log(s.toLowerCase());
+console.log(s.toUpperCase());
+
+console.log(s.includes("z"));
+console.log(s.includes("h"));
+
+console.log(s.concat(" ").concat(s2));
+
+
---
+++ Day2/Operators/app.js
@@ -0,0 +1,62 @@
+ var a = 10
+ var b = 3
+
+ // console.log(a+b) // adition
+ var sum = a + b
+ console.log(sum);
+
+ // console.log(a-b) // Subtraction
+ var diff = a-b
+ console.log(diff);
+ 
+ // console.log(a*b) // Multiplication
+ var prod = a * b
+ console.log(prod);
+
+ // console.log(a / b) // Quotient
+ var quo = a/b
+ console.log(quo);
+
+ // console.log(a % b) // Remainder
+ var rem = a % b
+ console.log(rem);
+
+ //console.log(a ** b) // Power
+ var pow = a ** b
+ console.log(pow);
+ 
+
+ console.log(typeof "");
+ console.log(typeof " ");
+
+ var str1 = "Hello"
+ var str2 = "world"
+
+ var sum = str1 + " " + str2
+
+ console.log(sum);
+ 
+ var age = 22
+ console.log(age);
+ 
+ age = age + 1
+ console.log(age);
+ 
+ var money = 100
+ console.log(money);
+ 
+ money = money - 20
+ console.log(money);
+
+
+
+ let something = 20
+ something++
+ console.log(something);
+
+ let something2 = 30
+ something2--
+ console.log(something2);
+ 
+ 
+ 
