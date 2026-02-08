10429d5130ca254bfb1667cf74669166dd4e311c
Adya Bharatdwaj <adyabharatdwaj@gmail.com>
2026-02-08 09:07:13 +0530

    Add files via upload

---
+++ Day3/Conditionals/app.js
@@ -0,0 +1,40 @@
+var age = 20
+if( age >= 18)
+{
+console.log("Allowed");
+}
+
+
+if(!true)
+
+{
+    console.log("Hello ji");   
+}
+
+if(!false)
+{
+    console.log("Adya"); 
+}
+
+
+var age = 10
+if(age>= 18)
+{
+    console.log("Allowed");
+}
+
+else
+{
+    console.log("Not Allowed");
+}
+
+
+var n = 96
+if (n % 2 == 0) 
+{
+  console.log("Even"); 
+}
+else
+{
+    console.log("Odd"); 
+}
---
+++ Day3/Operator/app.js
@@ -0,0 +1,132 @@
+//Assignment operators
+
+var age = 22
+age+= 1  // age = age + 1
+age+= 1
+age+= 1
+console.log(age);
+
+
+var paisa = 100
+// var paisa = paisa - 1
+// var paisa = paisa - 1
+// var paisa = paisa - 1
+// var paisa = paisa - 1
+// var paisa = paisa - 1
+var paisa = paisa - 10
+console.log(paisa);
+
+
+age = 22
+age-= 2  //age = age - 2
+console.log(age);
+
+age = 22
+age*= 2  //age = age * 2
+console.log(age);
+
+age = 22
+age/= 2  //age = age / 2
+console.log(age);
+
+age = 22
+age%= 2  //age = age % 2
+console.log(age);
+
+var age = 3
+age**= 4
+console.log(age);
+
+
+//increment, decrement operators (Arithemetic)
+
+var age = 80
+console.log(age);
+
+console.log(++age); //pre increment
+
+console.log(age++); //post increment
+console.log(age);
+
+var age = 22
+
+console.log(--age) //pre decrement
+
+console.log(age--); //post decrement
+console.log(age);
+
+
+var num = 10
+//pre
+
+// var newNumber = ++num
+var newNumber = --num
+console.log(num);
+console.log(newNumber);
+
+var Num = 20
+//post
+
+var newNumber = Num++
+console.log(Num);
+console.log(newNumber);
+
+
+//Comparison Operators
+
+var a = 10
+var b = 10
+console.log(a==b);
+console.log(a!=b);
+
+
+var moneywithAmit = 100
+var moneywithPankaj = 80
+
+console.log(moneywithAmit>moneywithPankaj);
+console.log(moneywithAmit<moneywithPankaj);
+
+console.log(moneywithPankaj>moneywithAmit);
+console.log(moneywithPankaj<moneywithAmit);
+
+
+var currentage = 18
+var eligibleage= 18
+
+console.log(currentage>=eligibleage);
+console.log(currentage<=eligibleage);
+
+var n1 = 55
+var n2 = "55"
+console.log(n1==n2);
+console.log(n1===n2);
+
+console.log(n1!==n2);
+// console.log(n1<==n2);
+// console.log(n1>==n2);
+
+
+console.log(10>4 && 8>3);
+console.log(4>10 && 8>3);
+console.log(10>4 && 3>8);
+console.log(4>10 && 3>8);
+
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false || true);
+// console.log(false || false);
+
+console.log(!true);
+console.log(!false);
+
+
+
+
+
